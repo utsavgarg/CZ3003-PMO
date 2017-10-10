cda34b645cd0d5b81c2d2bb2cc5e44169178feff
RomainN <rn@q3-technology.com>
2017-10-10 23:27:56 +0800

    right Chat

--- Chat/src/main/java/com/example/demo/config/WebSocketConfig.java
+++ Chat/src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.example.websocketdemo.config;
+package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
--- Chat/src/test/java/com/example/demo/DemoApplicationTests.java
+++ Chat/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.websocketdemo;
+package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WebsocketDemoApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
