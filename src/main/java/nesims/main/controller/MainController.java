b99cbb2559ac3bf08f8c7e5804278cae29831fd2
Derrick <derrickpehjh@gmail.com>
2017-10-18 22:46:04 +0800

    Changed Controller method types

--- src/main/java/nesims/main/controller/MainController.java
+++ src/main/java/nesims/main/controller/MainController.java
@@ -7,22 +7,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 	
 	public static final String REST_SERVICE_URI = "http://localhost:8080/api";
 	
-	@GetMapping("/login")
-	public String home() {
-		return "login";
-	}
+	@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+	public ModelAndView login() {
+	    ModelAndView mav = new ModelAndView();
+	    mav.setViewName("login");
+	    return mav;
+        }
 	
-	@GetMapping("/dashboard")
-	public String mainPage(ModelMap model) {
+	@RequestMapping(value={"/dashboard"}, method = RequestMethod.GET)
+	public ModelAndView dashboard(ModelMap model) {
 		
-		int i = 1;
+	    ModelAndView mav = new ModelAndView();
+	    mav.setViewName("dashboard");
+	    
+	    int i = 1;
 
 		RestTemplate restTemplate = new RestTemplate();
 		List<LinkedHashMap<String, Object>> reportsMap = restTemplate.getForObject(REST_SERVICE_URI + "/report/",
@@ -49,12 +57,16 @@ public class MainController {
 		} else {
 			System.out.println("No report exist----------");
 		}
-		return "dashboard";
-	}
+	    return mav;
+        }
 	
-	@GetMapping("/chat")
-	public String chatPage(ModelMap model) {
-		int i = 1;
+	@RequestMapping(value={"/chat"}, method = RequestMethod.GET)
+	public ModelAndView chat(ModelMap model) {
+		
+	    ModelAndView mav = new ModelAndView();
+	    mav.setViewName("chat");
+	    
+	    int i = 1;
 
 		RestTemplate restTemplate = new RestTemplate();
 		List<LinkedHashMap<String, Object>> reportsMap = restTemplate.getForObject(REST_SERVICE_URI + "/report/",
@@ -81,20 +93,25 @@ public class MainController {
 		} else {
 			System.out.println("No report exist----------");
 		}
-		return "chat";
-	}
+	    return mav;
+        }
 	
-	@GetMapping("/contacts")
-	public String contactPage() {
+	
+	@RequestMapping(value={"contacts"}, method = RequestMethod.GET)
+	public ModelAndView contacts() {
+	    ModelAndView mav = new ModelAndView();
+	    mav.setViewName("contacts");
+	    return mav;
+        }
+
+	
+	@RequestMapping(value={"/generate"}, method = RequestMethod.GET)
+	public ModelAndView generate(ModelMap model) {
 		
-		return "contacts";
-	}
-	
-	
-	@GetMapping("/generate")
-	public String generatePage(ModelMap model) {
-		
-		int i = 1;
+	    ModelAndView mav = new ModelAndView();
+	    mav.setViewName("generate");
+	    
+	    int i = 1;
 
 		RestTemplate restTemplate = new RestTemplate();
 		List<LinkedHashMap<String, Object>> reportsMap = restTemplate.getForObject(REST_SERVICE_URI + "/report/",
@@ -121,7 +138,8 @@ public class MainController {
 		} else {
 			System.out.println("No report exist----------");
 		}
-		return "generate";
-	}
+	    return mav;
+        }
+	
 
 }
