ecb561a80059f75df7723e34f9608c246baceb51
braveducky <darkskycloud@gmail.com>
2017-10-19 22:48:44 +0800

    Added Login/Logout (Added Missing file)

---
+++ src/main/java/nesims/main/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package nesims.main.config;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/CMOtoPMO/**").permitAll().anyRequest().authenticated().and().formLogin()
+				.loginPage("/login").defaultSuccessUrl("/dashboard").permitAll().and().logout().permitAll();
+	
+	}
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username,password, enabled from users where username=?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+	}
+	
+	
+
+/*	Ignore any request that starts with “/css/”. This is similar to configuring http@security=none when using the XML namespace configuration.
+*/	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**");
+		web.ignoring().antMatchers("/images/**");
+		web.ignoring().antMatchers("/js/**");
+		web.ignoring().antMatchers("/vendor/**");
+		
+
+
+	}
+}
